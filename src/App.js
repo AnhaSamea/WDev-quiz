ab325f56f6e7aeda83a2157127c527f8e5c24e7b
anhasamea <anhasamea707@gmail.com>
2022-10-11 13:31:14 +0600

    router created

--- src/App.js
+++ src/App.js
@@ -1,11 +1,34 @@
-
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import './App.css';
+import Blogs from './components/Blogs/Blogs';
+import Home from './components/Home/Home';
+import Main from './layout/Main';
+import Statistics from './Statistics/Statistics';
 
 function App() {
- 
+  const router = createBrowserRouter([
+    {
+      path: '/', element: <Main></Main>, children: [
+        {
+          path: '/',
+          loader: fetch('https://openapi.programming-hero.com/api/quiz'),
+          element: <Home></Home>
+        },
+        {
+          path: '/statistics',element: <Statistics></Statistics>
+        },
+        {
+          path: '/blogs' ,element: <Blogs></Blogs>
+        }
+      ]
+    },
+    {
+      path: '*', element: <div>This route is not found: 404</div>
+    }
+  ]);
   return (
     <div className="App">
-     <h2>HEyyy</h2>
+      <RouterProvider router={router}></RouterProvider>
     </div>
   );
 }
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,10 +1,15 @@
 import React from 'react';
-
+import { Link } from "react-router-dom";
 const Header = () => {
     return (
-        <div>
-            <h2>This is header</h2>
-        </div>
+        <nav className='flex bg-lime-400 p-5 justify-between text-lime-900'>
+            <h2 className='font-bold text-3xl'>WDev Quiz</h2>
+            <div>
+            <Link className='p-3' to='/'>Home</Link>
+            <Link className='p-3' to='/statistics'>Statistics</Link>
+            <Link className='p-3' to='/blogs'>Blogs</Link>
+            </div>
+        </nav>
     );
 };
 
