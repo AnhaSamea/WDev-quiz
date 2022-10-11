42d69f65e05843b9c2a6c9ac7129b551819dc165
anhasamea <anhasamea707@gmail.com>
2022-10-12 01:40:38 +0600

    style applied

--- src/components/Blogs/Blogs.js
+++ src/components/Blogs/Blogs.js
@@ -1,10 +1,22 @@
 import React from 'react';
-
+import './Blogs.css'
 const Blogs = () => {
     return (
-        <div>
-            <h2>This is blogs</h2>
-            <h3>what is the purpose of react router</h3>
+        <div className='blogs text-lime-900'>
+           
+            <div className='bg-lime-200 m-12 p-6 rounded-lg'>
+            <h3 className='text-2xl font-bold'>1. What is the purpose of react router?</h3>
+            <p><strong>Answer:</strong></p>
+            </div>
+            <div className='bg-lime-200 m-12 p-6 rounded-lg'>
+            <h3 className='text-2xl font-bold'>2. How does context api works?</h3>
+            <p><strong>Answer:</strong></p>
+            </div>
+            <div className='bg-lime-200 m-12 p-6 rounded-lg'>
+                <h3 className='text-2xl font-bold'>3. What is useRef hook?</h3>
+                <p><strong>Answer:</strong></p>
+            </div>
+
         </div>
     );
 };
