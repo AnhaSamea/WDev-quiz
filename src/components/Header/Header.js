1a379b5387ce51ea6a1ed1757b8e5386f8ab6375
anhasamea <anhasamea707@gmail.com>
2022-10-12 17:58:56 +0600

    nav bar responsived

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import { Link } from "react-router-dom";
+import './Header.css'
 const Header = () => {
     return (
-        <nav className='flex bg-lime-900 p-4 justify-between text-lime-50'>
+        <nav className='nav flex bg-lime-900 p-4 justify-between text-lime-50'>
             <h2 className='font-bold text-3xl'>WDev Quiz</h2>
+
             <div>
-            <Link className='p-3' to='/'>Home</Link>
-            <Link className='p-3' to='/statistics'>Statistics</Link>
-            <Link className='p-3' to='/blogs'>Blogs</Link>
+                <Link className='p-3' to='/'>Home</Link>
+                <Link className='p-3' to='/statistics'>Statistics</Link>
+                <Link className='p-3' to='/blogs'>Blogs</Link>
             </div>
+            
+
         </nav>
     );
 };
